8a0b6af424840183ef563cb1479ccf3c8386ef01
dhkaneda <dennishuynh@protonmail.com>
2021-05-28 20:24:43 -0700

    Add option to remove deprecation warning

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose =  require("mongoose");
 
-module.exports = mongoose.connect(process.env.mongoURI, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+module.exports = mongoose.connect(process.env.mongoURI, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
 // .then(res => console.log(`Connection Succesful ${res}`))
 .catch(err => console.log(`Error in DB connection ${err}`));
